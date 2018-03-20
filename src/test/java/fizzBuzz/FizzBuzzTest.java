acf141cf4568659be2a5fdeea8fedd28419e57f5
Lau Llobet <lau.llobet@gmail.com>
2018-03-20 12:02:38 +0100

    failing test

--- src/test/java/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/fizzBuzz/FizzBuzzTest.java
@@ -7,9 +7,20 @@ public class FizzBuzzTest {
     @Test
     public void forNumber1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals(fizzBuzz.doIt(1), 1);
+
+        int answer = fizzBuzz.doIt(1);
+
+        assertEquals(answer, 1);
     }
 
+    @Test
+    public  void forNumber2() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        int answer = fizzBuzz.doIt(2);
+
+        assertEquals(answer,2);
+    }
 /*    @Test(expected = Exception.class)
     public void catches() throws Exception {
         FizzBuzz k = new FizzBuzz();
