890c406e0d2e2fe8de00f89e599c4ae8baa41620
Lau Llobet <lau.llobet@gmail.com>
2018-03-20 12:00:30 +0100

    refactoring/cleaning

--- src/test/java/fizzBuzz/FizzBuzzTest.java
+++ src/test/java/fizzBuzz/FizzBuzzTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void asserts() throws Exception {
+    public void forNumber1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals(fizzBuzz.doIt(1), 1);
     }
 
-    @Test(expected = Exception.class)
+/*    @Test(expected = Exception.class)
     public void catches() throws Exception {
         FizzBuzz k = new FizzBuzz();
         throw new Exception();
-    }
+    }*/
 }
